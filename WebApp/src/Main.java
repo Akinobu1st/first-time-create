2c2020ef5594fa5e4fe2bf4f5df3597a5d5103f5
masumotokenta <58218709+masumotokenta@users.noreply.github.com>
2019-12-22 20:06:20 +0900

    TIME 作成

--- WebApp/src/Main.java
+++ WebApp/src/Main.java
@@ -1,6 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 //a
 public class Main {
 	public static void main (String[] args) {
-
+		Date now = new Date();
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		
 	}
 }
