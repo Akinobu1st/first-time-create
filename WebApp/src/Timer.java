de3a84123233fd2acab119fac74f63de15c68d79
Akinobu1st <58221904+Akinobu1st@users.noreply.github.com>
2020-01-29 13:31:44 +0900

    無視して

--- WebApp/src/Timer.java
+++ WebApp/src/Timer.java
@@ -1,4 +1,18 @@
+import java.io.Serializable;
+import java.awt.event.ActionListener;
 
-public class Timer{
+
+public class Timer extends Object implements Serializable{
+	 int delay = 1000; //milliseconds
+	  ActionListener taskPerformer = new ActionListener() {
+	      public void actionPerformed(ActionEvent evt) {
+	          //...Perform a task...
+	      }
+	  }
+	  
+	 
+	  new Timer(delay, taskPerformer).start();
+	  
+	  new Timer(delay, taskPerfomer).stop();
 
 }
