b33bc52960044f54cbfdb4338072c08e66837e15
Akinobu1st <58221904+Akinobu1st@users.noreply.github.com>
2020-01-31 15:10:13 +0900

    Time class

--- WebApp/src/Time.java
+++ WebApp/src/Time.java
@@ -2,30 +2,43 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Time{
-	
-	
-	Date date = new Date();
+	static  Date date1;
+	static  Date date2;
+	static long nowStart1;
+	 static long nowStop2;
+
 
 	 static void timeSet1() {
-		 Date date = new Date();
+
+		 Date date1 = new Date();
         SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy/MM/dd/ HH:mm:ss");
-        System.out.println(sdf1.format(date));
-        long sdf1Start = Date.getTime();
-        
-        
+        System.out.println(sdf1.format(date1));
+
+
+
         	}
 
-	static void timeSet2() {
-		Date date = new Date();
+	 static void timeSet2() {
+
+		Date date2 = new Date();
 		 SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		 System.out.println(sdf2.format(date));
+		 System.out.println(sdf2.format(date2));
 	 }
-	 
-	 static SimpleDateFormat timer(SimpleDateFormat sdf1,SimpleDateFormat sdf2 ) {
-		 SimpleDateFormat getTime = sdf1-sdf2;
+
+	 static long timer(long nowStart1, long nowStop2 ) {
+
+		 nowStart1 =  date1.getTime();
+		 nowStop2 =  date2.getTime();
+
+		long timeDiff = (nowStop2 - nowStart1);
+
+
+		 return timeDiff;
+
+
 	 }
 	 }
-        
-	
+
+
 
 
