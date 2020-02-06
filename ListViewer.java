a3d2d5817b2bc0ccf015df74143bdfbbb7a99e29
Akinobu1st <58221904+Akinobu1st@users.noreply.github.com>
2020-02-07 03:37:05 +0900

    Mainで呼び出せない、sencebilderとの連携不一致 orローカル側でのパスの不一致

---
+++ ListViewer.java
@@ -0,0 +1,22 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ListViewer {
+	@FXML
+	    private ListView<?> ListView;
+
+	 @FXML
+	 public static void onList1(ActionEvent e) {
+		 Time.timeSet1();
+
+
+	 }
+
+	 @FXML
+	 public static void onList2(ActionEvent e) {
+		 Time.timeSet2();
+
+
+	 }
+}
